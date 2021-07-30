a02fe5a49d00e47ed9e6efcc2e4e3dff9bd388cd
Christopher Gallo <cjg100@georgetown.edu>
2021-07-29 23:47:38 -0400

    setting up socket

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main() {
 
+    // declaring socket
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // error checking
+    if(socket_fd < 0) throw std::runtime_error("unspecified error for socket failure");
+
+    
+
+
 
     return 0;
 }
